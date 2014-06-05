09b50fa88d87e8cdb4aee2030b43e6bae0577364
Bozhidar Batsov <bozhidar@batsov.com>
2014-06-05 09:17:41 +0300

    [Fix #12] Merge mode code in a single file

--- swift-mode.el
+++ swift-mode.el
@@ -37,6 +37,151 @@
       "Set variable VAR to value VAL in current buffer."
       (list 'set (list 'make-local-variable (list 'quote var)) val))))
 
+(defgroup swift nil
+  "Configuration for swift-mode."
+  :group 'languages
+  :prefix "swift-")
+
+(defcustom swift-indent-offset 4
+  "Defines the indentation offset for Swift code."
+  :group 'swift
+  :type 'integer)
+
+(defun swift-indent--paren-level ()
+  "Return the paren level at point."
+  (nth 0 (syntax-ppss)))
+
+(defun swift-indent--in-str-or-cmnt ()
+  "Non-nil if point is in a string or comment."
+  (nth 8 (syntax-ppss)))
+
+(defun swift-indent--back-to-start-of-level ()
+  "Move backwards up to the start of the current indentation level."
+  (let ((current-level (swift-indent--paren-level)))
+    (back-to-indentation)
+    (while (> (swift-indent--paren-level) current-level)
+      (backward-up-list)
+      (back-to-indentation))))
+
+(defun swift-indent--rewind-past-str-cmnt ()
+  (goto-char (nth 8 (syntax-ppss))))
+
+(defun swift-indent--rewind-irrelevant ()
+  (let ((starting (point)))
+    (skip-chars-backward "[:space:]\n")
+    (if (looking-back "\\*/") (backward-char))
+    (if (swift-indent--in-str-or-cmnt)
+        (swift-indent--rewind-past-str-cmnt))
+    (if (/= starting (point))
+        (swift-indent--rewind-irrelevant))))
+
+(defun swift-indent--align-to-expr-after-brace ()
+  (save-excursion
+    (forward-char)
+    ;; We don't want to indent out to the open bracket if the
+    ;; open bracket ends the line.
+    (when (not (looking-at "[[:blank:]]*\\(?://.*\\)?$"))
+      (when (looking-at "[[:space:]]")
+        (forward-word 1)
+        (backward-word 1))
+      (current-column))))
+
+(defun swift-indent--rewind-to-beginning-of-current-level-expr ()
+  (let ((current-level (swift-indent--paren-level)))
+    (back-to-indentation)
+    (while (> (swift-indent--paren-level) current-level)
+      (backward-up-list)
+      (back-to-indentation))))
+
+(defun swift-indent--calculate-indentation ()
+  "Calculate the indentation column to use for `swift-indent-line'.
+Returns the column number as an integer."
+  (save-excursion
+    (back-to-indentation)
+    ;; Point is now at beginning of line.
+    (let* ((level (swift-indent--paren-level))
+           ;; Our "baseline" is one level out from the indentation of the
+           ;; expression containing the innermost enclosing opening bracket.
+           ;; That way if we are within a block that has a different indentation
+           ;; than this mode would give it, we still indent the inside of it
+           ;; correctly relative to the outside.
+           (baseline
+            (if (zerop level)
+                0
+              (save-excursion
+                (backward-up-list)
+                (swift-indent--back-to-start-of-level)
+                (+ (current-column) swift-indent-offset)))))
+      (cond
+       ;; A function return type is indented to the corresponding function arguments
+       ((looking-at "->")
+        (save-excursion
+          (backward-list)
+          (or (swift-indent--align-to-expr-after-brace)
+              (+ baseline swift-indent-offset))))
+
+       ;; A closing brace is 1 level unindented
+       ((looking-at "}") (- baseline swift-indent-offset))
+
+       ;; Doc comments in /** style with leading * indent to line up the *s
+       ((and (nth 4 (syntax-ppss)) (looking-at "*"))
+        (+ 1 baseline))
+
+       ;; If we're in any other token-tree / sexp, then:
+       (t
+        (or
+         ;; If we are inside a pair of braces, with something after the
+         ;; open brace on the same line and ending with a comma, treat
+         ;; it as fields and align them.
+         (when (> level 0)
+           (save-excursion
+             (swift-indent--rewind-irrelevant)
+             (backward-up-list)
+             ;; Point is now at the beginning of the containing set of braces
+             (swift-indent--align-to-expr-after-brace)))
+
+         (progn
+           (back-to-indentation)
+           ;; Point is now at the beginning of the current line
+           (cond
+            ;; If this line begins with "else" or "{", stay on the
+            ;; baseline as well (we are continuing an expression,
+            ;; but the "else" or "{" should align with the beginning
+            ;; of the expression it's in.)
+            ((looking-at (rx (or (and bow "else" eow) "{")))
+             baseline)
+            ;; Cases are indented to the same level as the enclosing switch statement.
+            ((looking-at (rx bow (or "case" "default") eow))
+             (- baseline swift-indent-offset))
+
+            ;; If we are at the first line, no indentation is needed, so stay at baseline.
+            ((save-excursion
+               (swift-indent--rewind-irrelevant)
+               ;; Point is now at the end of the previous line
+               (= 1 (line-number-at-pos (point))))
+             baseline)
+
+            ((save-excursion
+               (swift-indent--rewind-irrelevant)
+               ;; Point is now at the end of the previous line
+               ;; If the previous line ends with any of these:
+               ;;     { ? : ( , ; [ }
+               ;; then we are at the beginning of an expression, so stay on the baseline.
+               (looking-back "[(,:;?[{}]\\|[^|]|"))
+             baseline)
+
+            (t
+             baseline)))))))))
+
+(defun swift-indent-line ()
+  "Indent the current line.  Also see `swift-indent-offset'."
+  (interactive "*")
+  (let ((indent (swift-indent--calculate-indentation)))
+    (if (<= (current-column) (current-indentation))
+        (indent-line-to indent)
+      (save-excursion
+        (indent-line-to indent)))))
+
 ;; Font lock.
 
 (defvar swift-mode--type-decl-keywords
