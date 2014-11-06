a3caf1d14009f262de1b118fa5d87de6b754ddaa
Chris Barrett <chris.d.barrett@me.com>
2014-11-06 18:54:52 +1300

    Change '_' character class to 'symbol constituent'
    Resolves 62

--- swift-mode.el
+++ swift-mode.el
@@ -590,7 +590,7 @@ You can send text to the REPL process from other buffers containing source.
     (modify-syntax-entry ?\\ "\\" table)
 
     ;; Additional symbols
-    (modify-syntax-entry ?_ "-" table)
+    (modify-syntax-entry ?_ "_" table)
     (modify-syntax-entry ?: "_" table)
 
     ;; Comments
