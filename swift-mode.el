d047843cef3228219cbf1ec1668d5e348790714e
uk-ar <yuuki.ari@gmail.com>
2015-08-30 10:19:55 +0900

    Fix token for :

--- swift-mode.el
+++ swift-mode.el
@@ -777,7 +777,7 @@ You can send text to the REPL process from other buffers containing source.
     (modify-syntax-entry ?_ "w" table)
     (modify-syntax-entry ?? "_" table)
     (modify-syntax-entry ?! "_" table)
-    (modify-syntax-entry ?: "_" table)
+    (modify-syntax-entry ?: "." table)
 
     ;; Comments
     (modify-syntax-entry ?/  ". 124b" table)
