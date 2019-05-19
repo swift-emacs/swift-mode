04c8a621e9844ef3f7d53c41b046f71aadb1a9d2
taku0 <mxxouy6x3m_github@tatapa.org>
2019-05-19 17:18:50 +0900

    Fix beginning-of-defun hanging at the beginning of buffer
    Fixes #157.
    
    We would like to thank @dabrahams for reporting this, and @danielmartin for
    suggesting the cause.

--- swift-mode-beginning-of-defun.el
+++ swift-mode-beginning-of-defun.el
@@ -92,8 +92,11 @@ and the region is not active."
             (setq result (swift-mode:beginning-of-defun-backward))
             (when (< (point) last-position)
               (setq arg (1- arg)))
-            (when (< 0 arg)
-              (swift-mode:backward-token-or-list))))
+            (when (and (< 0 arg)
+                       (eq 'outside-of-buffer
+                           (swift-mode:token:type
+                            (swift-mode:backward-token-or-list))))
+              (setq result nil))))
       ;; Moving forward
       (setq result (swift-mode:beginning-of-defun-forward))
       (when (and result (< pos (point)))
