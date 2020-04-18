2ab9ea1784a12a482ed9e3fb284b7a7658f40fff
taku0 <mxxouy6x3m_github@tatapa.org>
2020-04-18 15:17:23 +0900

    Bump version to 8.0.2

--- swift-mode-repl.el
+++ swift-mode-repl.el
@@ -9,7 +9,7 @@
 ;;       Arthur Evstifeev <lod@pisem.net>
 ;;       Michael Sanders <michael.sanders@fastmail.com>
 ;;
-;; Version: 8.0.1
+;; Version: 8.0.2
 ;; Package-Requires: ((emacs "24.4") (seq "2.3"))
 ;; Keywords: languages swift
 
