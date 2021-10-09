d39908a9e4150870e3c29adf47775d49944f0538
taku0 <mxxouy6x3m_github@tatapa.org>
2021-10-09 15:34:37 +0900

    Enable elisp linter

--- scripts/run_linter.sh
+++ scripts/run_linter.sh
@@ -3,4 +3,4 @@
 # Run linter.  Used in Makefile.
 
 ./scripts/invoke_eldev.sh emacs --version || exit 1
-find ./*.el test/*.el '!' -name '*autoloads.el' -exec ./scripts/invoke_eldev.sh lint '{}' '+'
+find ./*.el test/*.el '!' -name '*autoloads.el' -exec ./scripts/invoke_eldev.sh lint doc re elisp '{}' '+'
