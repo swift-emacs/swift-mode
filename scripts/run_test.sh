6beef52fc8e2b5e4224bb376d78d52f59548a261
taku0 <mxxouy6x3m_github@tatapa.org>
2023-05-07 16:14:04 +0900

    If byte compilation fails, exit test immediately

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -3,7 +3,7 @@
 # Run tests.  Used in Makefile.
 
 ./scripts/invoke_eldev.sh emacs --version || exit 1
-./scripts/invoke_eldev.sh compile --set=all
+./scripts/invoke_eldev.sh compile --set=all || exit 1
 ./scripts/invoke_eldev.sh emacs --batch -q \
   --eval "(add-to-list 'load-path \"$(readlink -f .)\")" \
   --eval "(add-to-list 'load-path \"$(readlink -f .)/test\")" \
