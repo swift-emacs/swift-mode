c6b31487ac98fc27eab24b050d4040e1ba1b442e
taku0 <mxxouy6x3m_github@tatapa.org>
2026-08-22 17:39:24 +0900

    Respect ELDEV_DIR in invoke_eldev.sh fallback

--- scripts/invoke_eldev.sh
+++ scripts/invoke_eldev.sh
@@ -2,18 +2,20 @@
 
 # A little wrapper for eldev.  Used in Makefile.
 # If ${ELDEV} is executable, use it.
-# Otherwise, download the eldev to .eldev/bin/eldev if not exists, then
+# Otherwise, download the eldev to ${ELDEV_DIR}/bin/eldev if not exists, then
 # execute it.
+
+ELDEV_DIR=${ELDEV_DIR:-.eldev}
 
 if command -v "${ELDEV}" > /dev/null
 then
     "${ELDEV}" "$@"
-elif command -v .eldev/bin/eldev > /dev/null
+elif command -v "${ELDEV_DIR}/bin/eldev" > /dev/null
 then
-    .eldev/bin/eldev "$@"
+    "${ELDEV_DIR}/bin/eldev" "$@"
 else
-    mkdir -p ./.eldev/bin || exit 1
-    curl -fsSL https://raw.github.com/doublep/eldev/master/bin/eldev -o .eldev/bin/eldev || exit 1
-    chmod a+x .eldev/bin/eldev || exit 1
-    .eldev/bin/eldev "$@"
+    mkdir -p "${ELDEV_DIR}/bin" || exit 1
+    curl -fsSL https://raw.github.com/doublep/eldev/master/bin/eldev -o "${ELDEV_DIR}/bin/eldev" || exit 1
+    chmod a+x "${ELDEV_DIR}/bin/eldev" || exit 1
+    "${ELDEV_DIR}/bin/eldev" "$@"
 fi
