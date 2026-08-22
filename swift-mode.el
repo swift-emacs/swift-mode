05dd504ab047853e278ae05fcf16ccff70a838b7
taku0 <mxxouy6x3m_github@tatapa.org>
2026-08-22 18:41:13 +0900

    Add which-func-functions hook buffer-locally

--- swift-mode.el
+++ swift-mode.el
@@ -240,11 +240,7 @@ Signal `scan-error' if it hits opening parentheses."
 
   (delete-overlay swift-mode:anchor-overlay)
 
-  (add-hook 'which-func-functions
-            (lambda ()
-              (when (equal (with-current-buffer (current-buffer) major-mode)
-                           'swift-mode)
-                (swift-mode:current-defun-name))))
+  (add-hook 'which-func-functions #'swift-mode:current-defun-name nil t)
   (setq-local add-log-current-defun-function #'swift-mode:current-defun-name)
 
   (swift-mode:setup-swift-testing))
