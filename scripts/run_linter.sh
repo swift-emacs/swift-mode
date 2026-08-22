85805dd6ef0d049a8eac08eb31a1b9c58b092500
taku0 <mxxouy6x3m_github@tatapa.org>
2026-08-22 17:31:22 +0900

    If byte compilation fails, exit linter immediately

--- scripts/run_linter.sh
+++ scripts/run_linter.sh
@@ -5,6 +5,6 @@
 ./scripts/invoke_eldev.sh emacs --version || exit 1
 find . -name '*.elc' -exec rm '{}' '+' || exit 1
 rm -f *-autoloads.el || exit 1
-./scripts/invoke_eldev.sh compile --set=all
+./scripts/invoke_eldev.sh compile --set=all || exit 1
 find ./*.el test/*.el '!' -name '*autoloads.el' -exec \
      ./scripts/invoke_eldev.sh lint doc re elisp '{}' '+'
